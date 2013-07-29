009146d43789b4bff0ab1af25386a793611f485e
Seth-Johnson <sethj0438@gmail.com>
2013-07-29 10:38:28 -0700

    remove stuff we don't want

--- SO-ChatBot/master.js
+++ SO-ChatBot/master.js
@@ -1681,7 +1681,7 @@ var commands = {
 			return 'I\'m not dead! Honest!';
 		}
 		bot.continue();
-		return 'And on this day, you shall paint eggs for a giant bunny.';
+		return 'I live!';
 	},
 
 	die : function () {
@@ -2894,118 +2894,6 @@ loadUsers();
 }());
 
 
-//warning: if you have more than 7 points of super-sentitive feminist delicacy,
-// don't read this file. treat it as a nice black box.
-
-//bitch in English is a noun, verb and adjective. interesting.
-bot.personality = {
-	bitchiness : 0,
-	thanks  : {
-		0   : [ 'You kiss-ass', 'Most welcome' ],
-		0.5 : [ 'Thank you for noticing', 'teehee' ],
-		1   : [ 'Took you long enough', 'My pleasure', "Don't mention it" ],
-	},
-	apologies : {
-		0   : [ 'What for?' ],
-		0.5 : [ 'It was nothing...', 'No worries' ],
-		1   : [ "You're forgiven. For now. Don't push it." ]
-	},
-	//what an incredible name
-	stuff : {
-		0   : [ "Life is just *perfect*", "What\'s there to bitch about, as long as I have *you*..." ],
-
-		1   : [ "Oh don't mind me, that isn't difficult at all..." ],
-		1.2 : [
-			"You don't appreciate me enough. Not that I need to be thanked.." ],
-		1.3 : [ 'The occasional "thanks" or "I\'m sorry" would be nice...' ],
-		2   : [
-			"*sigh* Remember laughter? I don't. You ripped it out of me. " +
-				'Heartless bastard.' ]
-	},
-	//TODO: add special map for special times of the month
-	insanity : {},
-
-	okayCommands : { hangman : true, help : true, info : true },
-	check : function ( name ) {
-		return !this.okayCommands.hasOwnProperty( name );
-	},
-
-	bitch : function () {
-		return this.getResp( this.stuff );
-	},
-
-	command : function () {
-		this.bitchiness += this.getDB();
-	},
-	thank     : function () { return this.unbitch( this.thanks ); },
-	apologize : function () { return this.unbitch( this.apologies ); },
-
-	unbitch : function ( map, delta ) {
-		var resp = this.getResp( map );
-
-		this.bitchiness -= ( delta || this.bitchiness );
-		return resp;
-	},
-	getResp : function ( map ) {
-		return map[
-			this.bitchiness.fallsAfter(
-				Object.keys(map).map(Number).sort() )
-		].random();
-	},
-
-	isABitch : function () {
-		return this.bitchiness >= 1;
-	},
-
-	looksLikeABitch : function () {
-		return false;
-	},
-
-	//db stands for "delta bitchiness"
-	getDB : function () {
-		return this.isThatTimeOfTheMonth() ? 0.075 : 0.025;
-	},
-
-	isThatTimeOfTheMonth : function () {
-		var day = (new Date).getDate();
-		//based on a true story
-		return day < 2 || day > 27;
-	}
-};
-
-//you see the loophole?
-bot.listen( /thank(s| you)/i, bot.personality.thank, bot.personality );
-bot.listen(
-	/(I('m| am))?\s*sorry/i,
-	bot.personality.apologize, bot.personality );
-bot.listen( /^bitch/i, bot.personality.bitch, bot.personality );
-
-
-(function () {
-var hammers = {
-	STOP  : 'HAMMERTIME!',
-	STAHP : 'HAMMAHTIME!',
-	HALT  : 'HAMMERZEIT!',
-	STOY  : 'ZABIVAT\' VREMYA!',
-	CAESUM: 'MALLEUS TEMPUS!'
-};
-
-// /(STOP|STAHP|...)[\.!\?]?$/
-var re = new RegExp(
-	'(' +
-		Object.keys(hammers).map(RegExp.escape).join('|') +
-	')[\\.!?]?$' );
-
-IO.register( 'input', function STOP ( msgObj ) {
-	var sentence = msgObj.content.toUpperCase(),
-		res = re.exec( sentence );
-
-	if ( res ) {
-		bot.adapter.out.add( hammers[res[1]], msgObj.room_id );
-	}
-});
-
-})();
 
 
 var cowsay = (function () {
