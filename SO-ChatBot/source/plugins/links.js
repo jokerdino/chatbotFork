a4bd8f6ac735399eead80bfe6881b73bf02da16c
Seth-Johnson <sethj0438@gmail.com>
2013-08-03 22:17:40 -0700

    add links command

---
+++ SO-ChatBot/source/plugins/links.js
@@ -0,0 +1,26 @@
+
+function() {
+var links = {
+    blackscreen: "http://askubuntu.com/questions/162075/my-computer-boots-to-a-black-screen-what-options-do-i-have-to-fix-it",
+    broadcom: "http://askubuntu.com/questions/55868/how-to-install-broadcom-wireless-drivers"
+};
+
+var sendLink = function( args ) {
+    if (args in links) {
+
+        return links[args];
+       
+    }
+    
+}
+
+bot.addCommand({  
+
+	name : 'link',
+	fun : sendLink,
+	permissions : {
+		del : 'NONE'
+	}
+	
+	});
+})();
