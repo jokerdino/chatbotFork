89035cbdeb76e244df98139938a66d6ca28f4d00
Seth-Johnson <sethj0438@gmail.com>
2013-11-04 20:00:52 -0800

    Added the link command, which I accidentally removed before

---
+++ SO-ChatBot/source/plugins/link.js
@@ -0,0 +1,25 @@
+var links = {
+    blackscreen: "http://askubuntu.com/questions/162075/my-computer-boots-to-a-black-screen-what-options-do-i-have-to-fix-it",
+    broadcom: "http://askubuntu.com/questions/55868/how-to-install-broadcom-wireless-drivers"
+    dependencies: "http://askubuntu.com/questions/140246/how-do-i-resolve-unmet-dependencies"
+};
+
+var sendLink = function( args ) {
+    if (args in links) {
+
+        return links[args];
+       
+    }
+    
+}
+
+bot.addCommand({  
+
+        name : 'link',
+        fun : sendLink,
+        permissions : {
+                del : 'NONE'
+        }
+        
+        });
+})();
