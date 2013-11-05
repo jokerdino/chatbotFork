e7be9b7f35a12dd20c0757ab8a3736ad41d72376
Seth-Johnson <sethj0438@gmail.com>
2013-11-04 20:03:00 -0800

    Recompiled

--- SO-ChatBot/master.js
+++ SO-ChatBot/master.js
@@ -4492,6 +4492,32 @@ loadCommands();
 }());
 
 ;
+var links = {
+    blackscreen: "http://askubuntu.com/questions/162075/my-computer-boots-to-a-black-screen-what-options-do-i-have-to-fix-it",
+    broadcom: "http://askubuntu.com/questions/55868/how-to-install-broadcom-wireless-drivers"
+    dependencies: "http://askubuntu.com/questions/140246/how-do-i-resolve-unmet-dependencies"
+};
+
+var sendLink = function( args ) {
+    if (args in links) {
+
+        return links[args];
+       
+    }
+    
+}
+
+bot.addCommand({  
+
+        name : 'link',
+        fun : sendLink,
+        permissions : {
+                del : 'NONE'
+        }
+        
+        });
+})();
+;
 (function () {
 "use strict";
 var ownerRoom = 17;
