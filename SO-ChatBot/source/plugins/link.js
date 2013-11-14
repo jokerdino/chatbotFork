6b0b4230dbbf186f103597794197575e3664d4b5
Barneedhar Vigneshwar <barneedhar@gmail.com>
2013-11-14 15:48:32 +0530

    Add link on how to report a bug

--- SO-ChatBot/source/plugins/link.js
+++ SO-ChatBot/source/plugins/link.js
@@ -2,6 +2,7 @@ var links = {
     blackscreen: "http://askubuntu.com/questions/162075/my-computer-boots-to-a-black-screen-what-options-do-i-have-to-fix-it",
     broadcom: "http://askubuntu.com/questions/55868/how-to-install-broadcom-wireless-drivers",
     dependencies: "http://askubuntu.com/questions/140246/how-do-i-resolve-unmet-dependencies"
+    reportbug: "http://askubuntu.com/questions/5121/how-do-i-report-a-bug"
 };
 
 var sendLink = function( args ) {
